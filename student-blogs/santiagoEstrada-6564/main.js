0a2904fd47a0d6d925ceea4a61a3a1222515b39e
santy01 <santiago01est@gmail.com>
2023-04-22 08:03:01 -0500

    Semana 04 Clase 03 anexo main.js y librerias de dayjs

---
+++ student-blogs/santiagoEstrada-6564/main.js
@@ -0,0 +1,33 @@
+import dayjs from "dayjs"
+import relativeTime from "dayjs/plugin/relativeTime.js"
+
+await import ("dayjs/locale/es-us.js")
+
+dayjs.locale("es-us")
+dayjs.extend(relativeTime)
+
+console.log(dayjs().format("YYYY-MM-DD"))
+
+
+function humanize(date){
+    const wrappedDate= dayjs(date)
+    //Diferencia entre fechas
+    const daysDiff= Math.abs(wrappedDate.diff(Date.now(),"days"));
+
+    const isCurrentYear = wrappedDate.year() === new Date().getFullYear();
+
+    if(!isCurrentYear){
+        return wrappedDate.format("MMMM DD, YYYY")
+    }else if(daysDiff<=30){
+        return wrappedDate.fromNow();
+    }else{
+        return wrappedDate.format("MMMM DD")
+    }
+}
+
+console.log(humanize(Date.now()))
+console.log(humanize(dayjs().subtract(5,"days")))
+console.log(humanize(dayjs().subtract(60,"days")))
+console.log(humanize(dayjs().subtract(500,"days")))
+console.log(humanize(dayjs().subtract(30,"days")))
+console.log(humanize(dayjs().subtract(300,"days")))
